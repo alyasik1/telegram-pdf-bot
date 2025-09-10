c27ca7145b4d4d209558d0c2dc3e868479b78d64
Arina <alyasik123@gmail.com>
2025-09-10 03:51:31 +0300

    Обновление проекта: новый бот с асинхронностью, кнопками и FastAPI

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# TOKEN теперь берется напрямую из переменных окружения Railway
-TOKEN = os.environ.get("TOK")  # Здесь "TOK" — имя переменной, которое вы задали в Railway
-
-# Если токен по какой-то причине не задан
-if not TOKEN:
-    raise ValueError("❌ Переменная окружения 'TOK' не найдена! Задайте токен в Railway.")
--- main.py
+++
@@ -1,6 +0,0 @@
-from PdfTxtBot import PDFBot
-from config import TOKEN
-
-if __name__=='__main__':
-    ob=PDFBot(TOKEN)
-    ob.app.run_polling()
